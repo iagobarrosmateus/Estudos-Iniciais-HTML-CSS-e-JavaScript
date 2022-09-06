cb7c3fd5358dc0321538f77323139482843bd8f8
Iago Barros Mateus <iagomateus.9@gmail.com>
2022-09-06 14:09:09 -0300

    Condições

---
+++ Javascript/Exercício 008.js
@@ -0,0 +1,9 @@
+//console.log('O console funcionou corretamente')
+var vel = 80.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel > 80){
+    console.log('Você foi multado')
+}else{
+    console.log('Você está no limete de velocidade')
+}
+
