402eba68ddeea99725055e6f8ed1426d91da5617
Iago Barros Mateus <iagomateus.9@gmail.com>
2022-09-08 18:31:42 -0300

    Modelo de Exercício

--- Javascript/Modelo/modelo.js
+++ Javascript/Modelo/modelo.js
@@ -3,13 +3,16 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-   // var hora = 22
+    //var hora = 22
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        img.src = 'Manha.png'
+        img.scr = 'Manha.png'
+        document.body.style.background = '#e2cd9f'
     }else if (hora >= 12 && hora <=18) {
-        img.scr = 'Tarde.png'
+        img.src = 'Tarde.png'        
+        document.body.style.background = '#b9846f'
     }else{
-        img.scr = 'Noite.png'
+        img.src = 'Noite.png'
+        document.body.style.background = '#515154'
     }
 }
